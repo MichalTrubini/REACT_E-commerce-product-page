eb263ba2de33901f6524d77ec1ff90f39db8042d
Michal Trubini <mchl.trubini@gmail.com>
2022-07-09 13:20:20 +0200

    mobile side menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,22 +2,26 @@
  import hamburger from '../images/icon-menu.svg';
  import cart from '../images/icon-cart.svg';
  import profileImage from '../images/image-avatar.png';
+ import close from '../images/icon-close.svg';
 
  import '../css/nav.css'
 
 const Nav = () => {
     return (
-        <nav className='nav margin-fix'>
+        <nav className='nav'>
             <div className='nav__left'>
                 <img className="nav__hamburger" src={hamburger} alt="menu" />
                 <img src={logo} alt="sneakers" />
-                <ul className='nav__list'>
-                    <li>Collections</li>
-                    <li>Men</li>
-                    <li>Women</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+                <div className='nav__list-wrapper'>
+                    <img className="nav__menu-close" src={close} alt="close menu" />
+                    <ul className='nav__list'>
+                        <li>Collections</li>
+                        <li>Men</li>
+                        <li>Women</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
             </div>
             <div className='nav__right'>
                 <img src={cart} alt="cart" />
